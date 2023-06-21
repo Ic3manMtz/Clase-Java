5c013da4c3672278cbbe14bca6b9d89bd3b3dd11
Ic3manKraken <molap96@gmail.com>
2023-06-21 15:38:43 -0600

    Ejercicio 2
    Complemento del ejercicio 2 de la lista de ejercicios de tipos de datos

--- Tipos de datos/Expresion.java
+++ Tipos de datos/Expresion.java
@@ -2,16 +2,27 @@ package Clases;
 
 public class Expresion {
 	public static void main(String[] args) {
-		int resultado = -16 + 32 / 4;
+		int resultado = 32 / 4;
+		resultado += -16;
 		System.out.println("El resultado de la expresión -16 + 32 / 4 es " + resultado);
 		
-		resultado = (48 - 7)% 11;
+		resultado = 0;
+		resultado = 48-7;
+		resultado %= 11;
 		System.out.println("El resultado de la expresión (48 - 7)% 11 es " + resultado);
 		
-		resultado = (18 + 2) / (17 - 12);
+		resultado = 0;
+		resultado = 18+2;
+		resultado /= 17 - 12;
 		System.out.println("El resultado de la expresión (18 + 2) / (17 - 12)  es " + resultado);
 		
-		resultado = 17 - 18 / 6 * 3 + 11% 4;
+		resultado = 0;
+		int division = 18/6;
+		int multiplicacion = division *3;
+		int modulo = 11 % 4;
+		int resta = 17 - multiplicacion;
+		int suma = resta + modulo;
+		resultado = suma;
 		System.out.println("El resultado de la expresión 17 - 18 / 6 * 3 + 11% 4 es " + resultado);
 	}
 }
